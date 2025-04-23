486110dcae8058519876c5c1a2ab76bc6fec212a
dragonkie <dragonkie@hotmail.ca>
2025-04-23 18:43:03 -0400

    Publishing first package

--- test.mjs
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    for (const ele of document.querySelectorAll('.pokemon[data-status]')) {
-        let conditions = ele.dataset.status.split(' ');
-         
-    }
-});
