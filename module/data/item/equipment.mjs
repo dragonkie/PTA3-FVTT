2c8cec5e757605aef0c1bd1369de8c595d7c5a2c
dragonkie <dragonkie@hotmail.ca>
2026-03-03 00:37:21 -0500

    added static fiuelds reference

--- module/data/item/equipment.mjs
+++ module/data/item/equipment.mjs
@@ -1,3 +1,11 @@
 import ItemData from "../item.mjs";
 
-export default class EquipmentData extends ItemData { }
+export default class EquipmentData extends ItemData {
+    static defineSchema() {
+        const schema = super.defineSchema();
+
+        schema.equipped = new this.fields.BooleanField({ initial: false, nullable: false, required: true });
+
+        return schema;
+    }
+}
