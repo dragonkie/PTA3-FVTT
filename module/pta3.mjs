63a83bc071ee23485774c88f09e5d6abd3baf2b1
dragonkie <dragonkie@hotmail.ca>
2026-04-11 14:53:55 -0400

    Removed debug hook

--- module/pta3.mjs
+++ module/pta3.mjs
@@ -98,7 +98,6 @@ Hooks.once('init', function () {
 Hooks.once('ready', function () {
     // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
     Hooks.on('hotbarDrop', (bar, data, slot) => createItemMacro(data, slot));
-    CONFIG.debug.hooks = true;
 });
 
 /* -------------------------------------------- */
